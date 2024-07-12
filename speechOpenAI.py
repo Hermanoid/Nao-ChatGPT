83e66492d854ae6a002e8dbc2c632cab95e51d19
Adnadatt <127004907+Adnadatt@users.noreply.github.com>
2024-07-12 10:37:41 -0600

    Update speechOpenAI.py

--- speechOpenAI.py
+++ speechOpenAI.py
@@ -41,22 +41,24 @@ def speak(mic,person):
 	print(person, mic)
 	while True:
 		with sr.Microphone(device_index=mic) as source:
-			print("Listening...")
+
 			r.adjust_for_ambient_noise(source)
+			
+			print("Listening...")
 			audio = r.listen(source)
 			print("Stop Listening")
+			
 			try:
+				# using google to transcribe the audio file to text
 				text = r.recognize_google(audio)
 				print("mic " + str(mic) + " " + person + " said: " + text)
 
 				# read current chat history
-				with open("C:\\venvProjects\\projectIshani\\flask-server\\X.txt", "r") as f:
+				with open("C:\\venvProjects\\projectIshani\\flask-server\\history.txt", "r") as f:
 					chat_history = json.load(f)
 
-
+				# keeps the chat history with ChatGPT
 				chat_history.append({'role': 'user', 'content': text})
-
-
 				completion = client.chat.completions.create(
 					model= MODEL,
 					messages= chat_history
@@ -68,14 +70,14 @@ def speak(mic,person):
 				chat_history.append({"role": "assistant", "content": response})
 
 				# Save the updated chat history back to the file
-				with open("C:\\venvProjects\\projectIshani\\flask-server\\X.txt", "w") as f:
+				with open("C:\\venvProjects\\projectIshani\\flask-server\\history.txt", "w") as f:
 					json.dump(chat_history, f)
 
-				with open("C:\\venvProjects\\projectIshani\\flask-server\\responseText.txt", "w") as f:
+				with open("C:\\venvProjects\\projectIshani\\flask-server\\response.txt", "w") as f:
 					f.write(response)
 
 			except Exception as e:
 				print(f"An error occurred: {e}")
+				
 		input("press enter to continue")
 		print(person, mic)
-
